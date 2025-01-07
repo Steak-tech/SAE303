44896eba5d88ae4a166693ca9525a5e4be5a1953
Loup Olivier <loup.olivier@gmail.com>
2025-01-07 12:52:07 +0100

    Ajout du pie chart

--- src/acquisitions.js
+++ src/acquisitions.js
@@ -4,15 +4,36 @@ import jsonData from '../data.json';
 (async function () {
     // Récupérer les données pertinentes
     const tableData = jsonData.find(entry => entry.type === "table" && entry.name === "results")?.data || [];
-    
+
+
+
+    let tab = tableData.filter(row=>row.name ==='Picat')
+    let tab2 =tableData.filter(row=>row.name ==='ACE')
+    let tab3 =tableData.filter(row=>row.name ==='Fun-sCOP-cad')
+    function quality(best){
+        let moyenne = []
+        let temp_moy = 0
+        let res_moy = 0
+        let incr =0
+        for(let i=0;i<best.length;i++){
+            if (best[i].status != 'UNKNOWN') {
+                temp_moy = temp_moy + parseInt(best[i].time)
+                incr ++
+            }
+            else {}
+        }
+        res_moy = temp_moy / incr
+        return res_moy
+    }
+
     function compte(data) {
         const solveurs = {};
-        
+
         data.forEach(row => {
             if (!solveurs[row.name]) {
                 solveurs[row.name] = 0;
             }
-            
+
             if (row.status === "SAT" || row.status === "UNSAT") {
                 solveurs[row.name]++;
             }
@@ -47,11 +68,11 @@ import jsonData from '../data.json';
        
         return familles;
     }
-    
+
 
     // Utiliser la fonction compte pour obtenir les données
     const satCounts = compte(tableData);
-    
+
     // Convertir l'objet en tableau de paires clé-valeur et trier par valeur décroissante
     const sortedEntries = Object.entries(satCounts).sort((a, b) => b[1] - a[1]);
 
@@ -135,6 +156,40 @@ import jsonData from '../data.json';
                                 
                             }
                         }
+                    }
+                }
+            }
+        }
+    );
+    new Chart(
+        document.getElementById('best'),
+        {
+            type: 'pie',
+            data: {
+                labels: [tab[0].name, tab2[0].name, tab3[0].name], // Les noms des solveurs triés
+                datasets: [
+                    {
+                        label: 'Temps moyen de résolution', // Titre de la légende
+                        data: [quality(tab), quality(tab2), quality(tab3)], // Nombre de SAT par solveur trié
+                        backgroundColor: [
+                            'rgb(255, 99, 132)',
+                            'rgb(54, 162, 235)',
+                            'rgb(255, 205, 86)'
+                        ], // Couleur des barres
+                        borderColor: 'black', // Bordure des barres
+                        borderWidth: 0.1
+                    }
+                ]
+            },
+            options: {
+                plugins: {
+                    title: {
+                        display: true,
+                        text: 'Temps de résolution moyen pour les trois meilleurs solveurs sur les problèmes où ils ont trouvé une solution' // Titre du graphique
+                    },
+                    subtitle : {
+                        display: true,
+                        text: 'Plus une zone est grande, moins le solveur à été rapide'
                     }
                 }
             }
